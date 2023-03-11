0590f24ca40e5e05beb9eb151f5d4383e24ac543
bttobi <bttobi123@gmail.com>
2023-03-11 21:13:57 +0100

    removed some files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-import { LogoNerdbord } from "../components/LogoNerdbord";
-
+import Article from "../components/Article"
 import styles from "../styles/index.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <LogoNerdbord />
-      <h1 className={inter.className}>
-        Recruitment task for Javascript Trainee
-      </h1>
-    </div>
+    <Article />
   );
 }
